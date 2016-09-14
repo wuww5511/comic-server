d4bbfa1d67321c3ab1d979f807530ad7e6c682e2
wuwweiwei <wuww5511@126.com>
2016-09-14 17:17:10 +0800

    抽离出各步骤，使主流程简洁易懂~

--- server/routes/ftl.js
+++ server/routes/ftl.js
@@ -9,52 +9,89 @@ var _commonDataUrl = _path.join(process.cwd(), "./data/data.json");
 
 _router.use(/\S+\.ftl/, function (_req, _res, _next) {
     
-    var _originalUrl = _req.originalUrl;
-    var _query = "";
+    var _urlData = _getUrlData(_req.originalUrl);
     
-    if(_originalUrl.indexOf('?') >= 0) {
-        _originalUrl = _req.originalUrl.split('?')[0];
-        _query = _req.originalUrl.split('?')[1];
-    }
+    var _data = _getInputData(_urlData.path, _urlData.query);
     
-    var _dataOUrl = '/' + _originalUrl.split('/').slice(3).join('/');
-    var _dataUrl = _path.join(process.cwd(), "./data" + _dataOUrl.slice(0, -3) + "json");
-    var _commonData = _getJson(_commonDataUrl);
+    _ftl(_path.join(process.cwd(), "WEB-INF/template/"))
+        ._$render( _urlData.path.slice(1), _data, _onRender.bind(null, _req, _res, _next));
     
-    var _data = _getJson(_dataUrl);
     
-    Object.assign(_commonData, _data);
-    
-    _commonData.params || (_commonData.params = {});
-    
-    Object.assign(_commonData.params, _querystring.parse(_query));
-    
-    _ftl(_path.join(process.cwd(), "WEB-INF/template/"))._$render( _originalUrl.slice(1),
-                                 _commonData,
-                                 function (_err, _html) {
-        if(_err) {
-            _res.end(_err);
-        }
-        else {
-            var _$ = _cheerio.load(_html);
-            if(_$('body').length > 0) {
-                _$('body').append('<script src="/server/public/socket.io.js"></script><script src="/server/public/main.js"></script>')
-            }
-            _res.setHeader("Content-Type", "text/html;charset=utf-8")
-            _res.end(new Buffer(_$.html()));
-        }
-    });
 });
 
+function _onRender (_req, _res, _next, _err, _html) {
+    
+    if(_err) {
+        _res.end(_err);
+    }
+    else {
+        var _$ = _cheerio.load(_html);
+        if(_$('body').length > 0) {
+            _$('body').append('<script src="/server/public/socket.io.js"></script><script src="/server/public/main.js"></script>')
+        }
+        _res.setHeader("Content-Type", "text/html;charset=utf-8")
+        _res.end(new Buffer(_$.html()));
+    }
+}
+
+/**
+ *  获取json文件中的数据。不使用缓存
+ */
 function _getJson (_url) {
     try{
         delete require.cache[require.resolve(_url)];
         return require(_url);
     }
     catch(_e) {
-        console.warn("warn:" + _e.message);
         return {};
     }
+}
+
+/**
+ *  获取URL中的数据
+ *  @return {Object}
+ *      -   path {String}
+ *      -   query {Object}
+ */
+function _getUrlData (_url) {
+    
+    var _query = "",
+        _path = _url;
+    
+    if(_url.indexOf('?') >= 0) {
+        _path = _url.split('?')[0];
+        _query = _url.split('?')[1];
+    }
+    
+    return {
+        path: _path,
+        query: _querystring.parse(_query)
+    };
+}
+
+/**
+ *  获取模板的输入数据
+ *  @param {String} URL中的path
+ *  @param {Object} URL中的query
+ *  @return {Object}
+ */
+function _getInputData (_pathname, _query) {
+    
+    var _specificUrl = _path.join(process.cwd(),
+                                "./data" + '/' + _pathname.split('/').slice(3).join('/').slice(0, -3) + "json");
+    
+    var _commonData = _getJson(_commonDataUrl);
+    
+    var _specificData = _getJson(_specificUrl);
+    
+    var _res = Object.assign(_commonData, _specificData);
+    
+    _res.params || (_res.params = {});
+    
+    Object.assign(_res.params, _query);
+    
+    return _res;
+    
 }
 
 module.exports = _router;
