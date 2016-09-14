bcd0bca24c6edb92f383a7b3c57b9d55a326d825
wuwweiwei <wuww5511@126.com>
2016-09-14 18:43:46 +0800

    增加自动打开浏览器功能；

--- server/index.js
+++ server/index.js
@@ -6,7 +6,10 @@ var
     _io = require('./routes/io'),
     _others = require('./routes/others'),
     _pathRouter = require('./routes/path'),
-    _ftlRouter = require('./routes/ftl');
+    _ftlRouter = require('./routes/ftl'),
+    _exec = (require("child_process")).exec;
+
+var _port = 3000;
 
 _app.set('view engine', 'ejs');
 _app.set('views', _path.join(__dirname, './views'));
@@ -26,7 +29,19 @@ _app.use('/server', _express.static(_path.join(__dirname, "..")));
 
 _app.use(_express.static(process.cwd()));
 
-_server.listen(3000);
+_server.listen(_port);
+
+_openBrowser("http://localhost:" + _port);
+
+function _openBrowser(target, callback) {
+      var map, opener;
+      map = {
+        'darwin': 'open',
+        'win32': 'start '
+      };
+      opener = map[process.platform] || 'xdg-open';
+      return _exec("" + opener + " " + target, callback);
+}
 
          
         
